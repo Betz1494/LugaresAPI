55dc75fd17076e609c455ba433e0f2a9b0462433
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2021-04-21 21:27:17 -0500

    Se crea el controlador LugaresController

---
+++ Controllers/LugaresController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LugaresAPI.Repository.IRepository;
+
+namespace LugaresAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LugaresController : Controller
+    {
+        private ILugarRepository repository;
+        private readonly IMapper mapper;
+
+        public LugaresController(ILugarRepository _repository, IMapper _mapper)
+        {
+            repository = _repository;
+            mapper = _mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ObtenerTodosLugares()
+        {
+            var ObjLugares = repository.ObtenerLugares();
+
+            return Ok(ObjLugares);
+        }
+    }
+}
