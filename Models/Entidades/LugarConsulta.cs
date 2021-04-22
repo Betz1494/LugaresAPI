2f4e9331168589fd7e9b1f6998143887d4dc6ae7
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2021-04-21 21:25:44 -0500

    Se Agrega la entidad LugarConsulta

---
+++ Models/Entidades/LugarConsulta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LugaresAPI.Models.Entidades
+{
+    public class LugarConsulta
+    {
+        
+        public int Id { get; set; }      
+        public string Nombre { get; set; }     
+        public string Localizacion { get; set; }
+        public DateTime Creacion { get; set; }
+        public byte[] Foto { get; set; }
+        public DateTime Inauguracion { get; set; }
+    }
+}
