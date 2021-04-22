1fdf77c847940cb7cc795ced952bd4ae8de4b804
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2021-04-21 21:26:25 -0500

    Se agrega clase del Mapping al Proyecto

---
+++ Mapper/LugaresMappings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LugaresAPI.Models;
+using LugaresAPI.Models.Entidades;
+
+namespace LugaresAPI.Mapper
+{
+    public class LugaresMappings : Profile
+    {
+        public LugaresMappings()
+        {
+            CreateMap<Lugar, LugarConsulta>().ReverseMap();
+        }
+
+    }
+}
