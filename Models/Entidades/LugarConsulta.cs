eec0ddafe63b1e5856bbbf8d6fb5aa24b3314288
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2021-04-23 20:43:10 -0500

    Se agerga DataAnnotations al modelo

--- Models/Entidades/LugarConsulta.cs
+++ Models/Entidades/LugarConsulta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace LugaresAPI.Models.Entidades
     public class LugarConsulta
     {      
         public int Id { get; set; }      
-        public string Nombre { get; set; }     
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
         public string Localizacion { get; set; }
         public DateTime Creacion { get; set; }
         public byte[] Foto { get; set; }
